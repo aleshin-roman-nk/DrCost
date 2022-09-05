fd538a37609f11c78a487ad8559c4bd39b997355
Roman Aleshin <aleshin.roman.nk@gmail.com>
2022-09-05 14:42:47 +0700

    first commit

---
+++ DrCost.Models/Budget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrCost.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal AllowedSum { get; set; }
+        public decimal Costs { get; set; }
+    }
+}
